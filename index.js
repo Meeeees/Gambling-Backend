8e2cd6dc8a45be29fe175522d0b369ba0f95492c
meeeeesgithub <119116083+meeeeesgithub@users.noreply.github.com>
2023-12-27 18:48:05 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(process.env.PORT || 8080, () => console.log('server running'))
